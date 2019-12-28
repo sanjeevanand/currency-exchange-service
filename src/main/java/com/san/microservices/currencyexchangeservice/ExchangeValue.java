696b650ff4f4f8d628a47d85ebf8ed487aa65925
sanjeevanand <maxsanjeevanand@gmail.com>
2019-12-28 22:31:56 +0530

    first commit1

---
+++ src/main/java/com/san/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,65 @@
+package com.san.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+	
+	
+	
+	@Id
+	private Long id;
+	@Column(name="exchange_from")
+	private String from;
+	@Column(name="exchange_to")
+	private String to;
+	private BigDecimal conversionMultiple;
+	private int port;
+	
+	public ExchangeValue(String from, String to, Long id, BigDecimal conversionMultiple) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.id = id;
+		this.conversionMultiple = conversionMultiple;
+	}
+	public ExchangeValue() {
+	//	super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value(id,exchange_from,exchange_to,conversion_multiple,port) values(1000,'USD','INR',65,0);
+insert into exchange_value(id,exchange_from,exchange_to,conversion_multiple,port) values(1001,'EUR','INR',75,0);
+insert into exchange_value(id,exchange_from,exchange_to,conversion_multiple,port) values(1002,'AUS','INR',25,0);
